a023066c0f20257e7fddc8e59aeab70c7601a392
Comondear <elcomondear@gmail.com>
2022-11-29 12:34:47 -0700

    token-fixed

--- src/components/FriendsList/FriendsItem.js
+++ src/components/FriendsList/FriendsItem.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import scss from './Friends.module.scss';
 
-export const FriendItem = ({ friend: { name, avatar, isOnline = ? `${scss.status} ${scss.online}`
-            : `${scss.status} ${scss.offline}` } }) => {
+export const FriendItem = ({ friend: { name, avatar, isOnline } }) => {
   return (
     <div className={scss.item}>
       <span
-        className={isOnline}
+        className={
+          isOnline
+            ? `${scss.status} ${scss.online}`
+            : `${scss.status} ${scss.offline}`
+        }
       ></span>
       <img className={scss.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={scss.name}>{name}</p>
